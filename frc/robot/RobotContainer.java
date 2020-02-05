80e7a5225513fd23d84080fdbda8b4c7ab10b5e3
Circuitree Laptop New <dontknowouremail@gmail.com>
2020-02-05 18:13:25 -0500

    Update RobotContainer.java

--- frc/robot/RobotContainer.java
+++ frc/robot/RobotContainer.java
@@ -16,10 +16,15 @@ import frc.robot.commands.climbupPnumaticCommand;
 import frc.robot.commands.closeShooterPnumaticCommand;
 import frc.robot.commands.drivetrainCommand;
 import frc.robot.commands.intakeSpitballMotorCommand;
+import frc.robot.commands.intakeTakeballMotorCommand;
+import frc.robot.commands.kickoutPnumaticCommand;
 import frc.robot.commands.openShooterPnumaticCommand;
+import frc.robot.commands.shootBallsFullMotorCommand;
+import frc.robot.commands.shootBallsHalfMotorCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.climbPnumaticSubsystem;
 import frc.robot.subsystems.drivetrainSubsystem;
+import frc.robot.subsystems.intakeMotorSubsystem;
 import frc.robot.subsystems.kickoutPnumaticSubsystem;
 import frc.robot.subsystems.shooterPnumaticSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -42,9 +47,7 @@ public class RobotContainer {
   private final climbPnumaticSubsystem m_climbPnumaticSubsystemSubsystem = new climbPnumaticSubsystem();
   private final kickoutPnumaticSubsystem m_kickoutPnumaticSubsystem = new kickoutPnumaticSubsystem();
   private final shooterPnumaticSubsystem m_shooterPnumaticSubsystem = new shooterPnumaticSubsystem();
-
-  //private intakeMotorSubsystem m_intakeMotorSubsystem = new intakeMotorSubsystem();
-
+  private final intakeMotorSubsystem m_intakeMotorSubsystem = new intakeMotorSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
@@ -55,6 +58,10 @@ public class RobotContainer {
   private final openShooterPnumaticCommand m_openShooterPnumaticCommand = new openShooterPnumaticCommand();
   private final closeShooterPnumaticCommand m_closeShooterPnumaticCommand = new closeShooterPnumaticCommand();
   private final intakeSpitballMotorCommand m_inIntakeSpitballMotorCommand = new intakeSpitballMotorCommand();
+  private final intakeTakeballMotorCommand m_inIntakeTakeballMotorCommand = new intakeTakeballMotorCommand();
+  private final kickoutPnumaticCommand m_kickoutPnumaticCommand = new kickoutPnumaticCommand();
+  private final shootBallsFullMotorCommand m_shootBallsFullMotorCommand = new  shootBallsFullMotorCommand();
+  private final shootBallsHalfMotorCommand m_shootBallsHalfMotorCommand = new  shootBallsHalfMotorCommand(); 
 
   public static Object driveRobot;
 
@@ -66,15 +73,66 @@ public class RobotContainer {
   public static XboxController Xbox2 = new XboxController(1);
   public static Joystick Fightstick = new Joystick(2);
   
-    JoystickButton O1 = new JoystickButton(Xbox1, 1);
+    JoystickButton O1 = new JoystickButton(Xbox1, 1); //Take Balls
+    JoystickButton O2 = new JoystickButton(Xbox1, 2); //Spit Balls
+    JoystickButton O3 = new JoystickButton(Xbox1, 3); 
+    JoystickButton O4 = new JoystickButton(Xbox1, 4);
+    JoystickButton O5 = new JoystickButton(Xbox1, 5);
+    JoystickButton O6 = new JoystickButton(Xbox1, 6);
+    JoystickButton O7 = new JoystickButton(Xbox1, 7);
+    JoystickButton O8 = new JoystickButton(Xbox1, 8);
+
     JoystickButton T1 = new JoystickButton(Xbox2, 1);
-    JoystickButton F1 = new JoystickButton(Fightstick, 1);
+    JoystickButton T2 = new JoystickButton(Xbox2, 2);
+    JoystickButton T3 = new JoystickButton(Xbox2, 3);
+    JoystickButton T4 = new JoystickButton(Xbox2, 4);
+    JoystickButton T5 = new JoystickButton(Xbox2, 5);
+    JoystickButton T6 = new JoystickButton(Xbox2, 6);
+    JoystickButton T7 = new JoystickButton(Xbox2, 7);
+    JoystickButton T8 = new JoystickButton(Xbox2, 8);
+
+    JoystickButton F1 = new JoystickButton(Fightstick, 1); 
+    JoystickButton F2 = new JoystickButton(Fightstick, 2); //Climb Up
+    JoystickButton F3 = new JoystickButton(Fightstick, 3); //Climb Down
+    JoystickButton F4 = new JoystickButton(Fightstick, 4); //Kickout Robot
+    JoystickButton F5 = new JoystickButton(Fightstick, 5);
+    JoystickButton F6 = new JoystickButton(Fightstick, 6);
+    JoystickButton F7 = new JoystickButton(Fightstick, 7);
+    JoystickButton F8 = new JoystickButton(Fightstick, 8);
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
     configureButtonBindings();
+
+    O1.toggleWhenPressed(new intakeTakeballMotorCommand());
+    O2.toggleWhenPressed(new intakeSpitballMotorCommand());
+    O3.toggleWhenPressed(m_autoCommand);
+    O4.toggleWhenPressed(m_autoCommand);
+    O5.toggleWhenPressed(m_autoCommand);
+    O6.toggleWhenPressed(m_autoCommand);
+    O7.toggleWhenPressed(m_autoCommand);
+    O8.toggleWhenPressed(m_autoCommand);
+
+    T1.toggleWhenPressed(m_autoCommand);
+    T2.toggleWhenPressed(m_autoCommand);
+    T3.toggleWhenPressed(m_autoCommand);
+    T4.toggleWhenPressed(m_autoCommand);
+    T5.toggleWhenPressed(m_autoCommand);
+    T6.toggleWhenPressed(m_autoCommand);
+    T7.toggleWhenPressed(m_autoCommand);
+    T8.toggleWhenPressed(m_autoCommand);
+
+    F1.toggleWhenPressed(new kickoutPnumaticCommand());
+    F2.toggleWhenPressed(new climbdownPnumaticCommand());
+    F3.toggleWhenPressed(new climbupPnumaticCommand());
+    F4.toggleWhenPressed(m_autoCommand);
+    F5.toggleWhenPressed(m_autoCommand);
+    F6.toggleWhenPressed(m_autoCommand);
+    F7.toggleWhenPressed(m_autoCommand);
+    F8.toggleWhenPressed(m_autoCommand);
+
   }
 
   /**
